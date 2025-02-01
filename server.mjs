e5b73f1bd11357bfc3eafd8d98c0c20f797c192a
あわわわとーにゅ <17376330+u1-liquid@users.noreply.github.com>
2025-02-02 07:39:48 +0900

    spec(fastify): set maxParamLength to 1024

---
+++ server.mjs
@@ -0,0 +1,26 @@
+import { fastify } from 'fastify';
+import closeWithGrace from 'close-with-grace';
+import config from './config.js';
+import app from './built/index.js';
+
+const fastifyInstance = fastify({
+    logger: true,
+    maxParamLength: 1024,
+});
+
+fastifyInstance.register(app, { ...config });
+
+// noinspection JSCheckFunctionSignatures
+closeWithGrace({ delay: process.env.FASTIFY_CLOSE_GRACE_DELAY || 500 }, async function ({ signal, err, manual }) {
+    if (err) {
+        fastifyInstance.log.error(err);
+    }
+    await fastifyInstance.close();
+});
+
+fastifyInstance.listen({ port: process.env.PORT || 3000 }, (err) => {
+    if (err) {
+        fastifyInstance.log.error(err);
+        process.exit(1);
+    }
+});
